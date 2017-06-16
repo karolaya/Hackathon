b18e7429a19e0bbe5097062f03a1d6cba2fe3ff2
Carlos Benavides <cebenavides@uninorte.edu.co>
2017-06-16 15:18:58 -0500

    More and more stuff

--- hackaton/app/controllers/students_controller.rb
+++ hackaton/app/controllers/students_controller.rb
@@ -5,9 +5,8 @@ class StudentsController < ApplicationController
   # GET /students
   # GET /students.json
   def index
-    @students = Student.all
-
-  
+    @institution = Institution.find(params[:institution_id])
+    @students = @institution.students
   end
 
   # GET /students/1
@@ -23,7 +22,8 @@ class StudentsController < ApplicationController
 
   # GET /students/new
   def new
-    @student = Student.new
+    @institution = Institution.find(params[:institution_id])
+    @student = @institution.students.build
   end
 
   # GET /students/1/edit
@@ -33,7 +33,8 @@ class StudentsController < ApplicationController
   # POST /students
   # POST /students.json
   def create
-    @student = Student.new(student_params)
+    @institution = Institution.find(params[:institution_id])
+    @student = @institution.students.build(student_params)
     
     @student.nota9a = @student.nota9a/20.0 unless @student.nota9a == nil
     @student.nota10a = @student.nota10a/20.0 unless @student.nota10a == nil
@@ -44,10 +45,10 @@ class StudentsController < ApplicationController
 
     respond_to do |format|
       if @student.save
-        format.html { redirect_to @student, notice: 'Student was successfully created.' }
+        format.html { redirect_to institution_students_path(@institution), notice: 'Student was successfully created.' }
         format.json { render :show, status: :created, location: @student }
       else
-        format.html { render :new }
+        format.html { render :action => "new" }
         format.json { render json: @student.errors, status: :unprocessable_entity }
       end
     end
@@ -56,12 +57,13 @@ class StudentsController < ApplicationController
   # PATCH/PUT /students/1
   # PATCH/PUT /students/1.json
   def update
+    @institution = Institution.find(params[:institution_id])
     respond_to do |format|
       if @student.update(student_params)
-        format.html { redirect_to @student, notice: 'Student was successfully updated.' }
+        format.html { redirect_to institution_students_path(@institution), notice: 'Student was successfully updated.' }
         format.json { render :show, status: :ok, location: @student }
       else
-        format.html { render :edit }
+        format.html { render :action => "edit" }
         format.json { render json: @student.errors, status: :unprocessable_entity }
       end
     end
@@ -70,9 +72,10 @@ class StudentsController < ApplicationController
   # DELETE /students/1
   # DELETE /students/1.json
   def destroy
+    @institution = Institution.find(params[:institution_id])
     @student.destroy
     respond_to do |format|
-      format.html { redirect_to students_url, notice: 'Student was successfully destroyed.' }
+      format.html { redirect_to institution_students_path(@institution), notice: 'Student was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -81,6 +84,7 @@ class StudentsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_student
       @student = Student.find(params[:id])
+      @institution = @student.institution
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- hackaton/config/routes.rb
+++ hackaton/config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   
   root :to => 'home#index'
 
-  resources :students
-  resources :institutions
+  resources :institutions do
+  	resources :students
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
