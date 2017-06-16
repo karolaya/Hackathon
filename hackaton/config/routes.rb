611e361cbbe1d438309fad509ed19c7b0d29c296
karolaya <karolayardila@hotmail.com>
2017-06-16 14:31:29 -0500

    Front 2

--- hackaton/config/routes.rb
+++ hackaton/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   devise_for :admins
   devise_for :users
   get 'main/index'
   
-  root :to => 'main#index'
+  root :to => 'home#index'
 
   resources :students
   resources :institutions
